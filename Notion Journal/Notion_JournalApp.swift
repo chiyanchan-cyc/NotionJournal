c92825e519256f43901a50af1225b2a9bc0246d3
Chi Yan Chan <chiyanchan@me.com>
2025-12-30 16:34:45 +0800

    Initial Commit

---
+++ Notion Journal/Notion_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notion_JournalApp.swift
+//  Notion Journal
+//
+//  Created by Mac on 2025/12/30.
+//
+
+import SwiftUI
+
+@main
+struct Notion_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
